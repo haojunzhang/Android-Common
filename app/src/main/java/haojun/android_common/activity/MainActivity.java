19e3f8758789838f91180bf8f40f83dca6d808c4
HaoJun <rin841005@gmail.com>
2017-05-10 23:10:11 +0800

    update EncrptyHelper
    add sha256

--- app/src/main/java/haojun/android_common/activity/MainActivity.java
+++ app/src/main/java/haojun/android_common/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import haojun.android_common.R;
+import haojun.android_common.helper.EncrptyHelper;
+import haojun.android_common.helper.LogHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String text = "123";
+        LogHelper.d(EncrptyHelper.sha256(text));
     }
 }
--- app/src/main/java/haojun/android_common/helper/EncrptyHelper.java
+++ app/src/main/java/haojun/android_common/helper/EncrptyHelper.java
@@ -28,5 +28,30 @@ public class EncrptyHelper {
         }
         return h[i / 16] + h[i % 16];
     }
+
+    public static String sha256(String s) {
+        MessageDigest sha;
+
+        try {
+            sha = MessageDigest.getInstance("SHA-256");
+            sha.update(s.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        return byte2hex(sha.digest());
+    }
+
+    private static String byte2hex(byte[] b) {
+        String hs = "";
+        String stmp = "";
+        for (int n = 0; n < b.length; n++) {
+            stmp = (java.lang.Integer.toHexString(b[n] & 0XFF));
+            if (stmp.length() == 1) hs = hs + "0" + stmp;
+            else hs = hs + stmp;
+        }
+        return hs.toUpperCase();
+    }
 }
 
