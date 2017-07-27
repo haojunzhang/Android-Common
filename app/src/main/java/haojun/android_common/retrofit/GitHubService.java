15662dbd3662e5ede5df1ee3755d3763e8af95a3
haojunzhang <rin841005@gmail.com>
2017-07-28 00:39:48 +0800

    add retrofit example

--- app/src/main/java/haojun/android_common/retrofit/GitHubService.java
+++ app/src/main/java/haojun/android_common/retrofit/GitHubService.java
@@ -7,6 +7,7 @@ import haojun.android_common.model.RepoModel;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
@@ -15,10 +16,25 @@ public interface GitHubService {
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://api.github.com/")
             .addConverterFactory(GsonConverterFactory.create())
+//            .addConverterFactory(ScalarsConverterFactory.create())
             .build();
 
     GitHubService service = retrofit.create(GitHubService.class);
 
     @GET("users/{user}/repos")
     Call<ArrayList<RepoModel>> getRepoList(@Path("user") String user);
+
+//    @Multipart
+//    @POST("Picture/Create")
+//    Call<Player> createPicture(@Part MultipartBody.Part fileName,
+//                               @Part MultipartBody.Part subFileName,
+//                               @Part MultipartBody.Part type,
+//                               @Part MultipartBody.Part file);
+//
+//    @POST("Player/SignIn")
+//    Call<Player> signIn(@Body Player player);
+//
+//    @GET("youbike")
+//    Call<String> getData();
+
 }
