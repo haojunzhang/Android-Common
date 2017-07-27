ad802d97d4afc85118f33f0946c52d45501d591b
haojunzhang <rin841005@gmail.com>
2017-07-27 23:10:19 +0800

    update gitignore, EncryptHelper

--- app/src/main/java/haojun/android_common/activity/MainActivity.java
+++ app/src/main/java/haojun/android_common/activity/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import haojun.android_common.R;
-import haojun.android_common.helper.EncrptyHelper;
+import haojun.android_common.helper.EncryptHelper;
 import haojun.android_common.helper.LogHelper;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String text = "123";
-        LogHelper.d(EncrptyHelper.sha256(text));
+        LogHelper.d(EncryptHelper.md5(text));
+        LogHelper.d(EncryptHelper.sha256(text));
+        LogHelper.d(EncryptHelper.sha512(text));
     }
 }
